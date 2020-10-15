76b803fb1bfac476a0af7cfdfa33b5c2ac021e4f
Manuela Fernandez <manu.fer.0396@gmail.com>
2020-10-14 22:58:01 -0300

    borro carpeta .vscode para debug y borro warning PDO

--- app/helpers/db.helper.php
+++ app/helpers/db.helper.php
@@ -4,9 +4,6 @@
 
         function connect() { 
             $db = new PDO('mysql:host=localhost;'.'dbname=db_recetas;charset=utf8', 'root', '');
-            //solo en mmodo desarrollo. quiero que pdo me muestre un warning
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-    
             return $db;
         }
     }
