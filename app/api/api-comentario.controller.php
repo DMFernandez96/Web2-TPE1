c813a2d6191766f76a595d7f40b4743f3e5b6411
Manuela Fernandez <manu.fer.0396@gmail.com>
2020-11-29 23:05:43 -0300

    agrego metodo borrar comentario en api controller

--- app/api/api-comentario.controller.php
+++ app/api/api-comentario.controller.php
@@ -1,18 +1,21 @@
 <?php
 require_once 'app/models/comentario.model.php';
 require_once 'app/api/api.view.php';
+include_once 'app/helpers/auth.helper.php';
 
 class ApiComentarioController{
 
     private $model;
     private $view;
     private $data;
+    private $authHelper;
    
     function __construct(){
         $this->model = new ComentarioModel();
         $this->view = new APIView();
         /**Obtengo lo que tengo por post, como texto */
         $this->data = file_get_contents('php://input');
+        $this->authHelper = new AuthHelper();
     }
 
     function getData(){ //lo transforma en un json
@@ -35,23 +38,37 @@ class ApiComentarioController{
     }
 
     public function add($params = null){
+        $this->authHelper->checkLogueado();
+
         $body = $this->getData(); //es lo que se envia por formulario
 
         $cuerpo        = $body->cuerpo;
-        $puntaje  = $body->puntuacion;
-        $id_receta      = $body->id_receta;
-        $id_usuario = $body->id_usuario;
+        $puntaje       = $body->puntuacion;
+        $id_receta     = $body->id_receta;
+        $id_usuario    = $body->id_usuario;
         
 
-        $id = $this->model->insert($cuerpo, $puntaje, $id_receta, $id_usuario);
+        $success = $this->model->insert($cuerpo, $puntaje, $id_receta, $id_usuario);
 
-        if($id > 0){
-            $this->view->response("Se agrego comentario $id exitosamente", 200);
+        if($success > 0){
+            $comentario = $this->model->get($success);
+            $this->view->response($comentario, 200); //devuelve el comentario ni bien lo inserta
+            /* $this->view->response("Se agrego comentario $success exitosamente", 200); */ 
         }else{
             $this->view->response("El comentario NO se pudo insertar", 500); 
         }
 
     }
+
+   function getComentariosReceta($params=null){
+        $idReceta = $params[':ID'];
+        $comentarios= $this->model->getComentariosReceta($idReceta);
+        if($comentarios > 0){
+            $this->view->response($comentarios, 200);
+        }else{
+            $this->view->response("No hay comentarios para esta receta", 404);
+        }
+   }
 
     function delete($params = null){
         $idComentario = $params[':ID'];
@@ -59,8 +76,17 @@ class ApiComentarioController{
         if($success){
             $this->view->response("El comentario con el id=$idComentario se borró exitosamente", 200);
         }else{
-            $this->view->response("El comentario con el id=$idComentario no existe", 404);
+            $this->view->response("El comentario id=$idComentario no existe", 404);
         }
+    }
+
+    function get($params=null){
+        $idComentario = $params[':ID'];
+        $comentario = $this->model->get($idComentario);
+        if ($comentario)
+            $this->view->response($comentario, 200);
+        else
+            $this->view->response("No existe el comentario con id= $idComentario", 404);
     }
 
     function show404($params= null){
