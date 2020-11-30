27bd82d27604a3652b51a51d786c890389a6fd12
Manuela Fernandez <manu.fer.0396@gmail.com>
2020-11-29 23:30:02 -0300

    borro codigo comentado en router-api que habia usado como prueba al comienzo

--- router-api.php
+++ router-api.php
@@ -1,21 +1,23 @@
 <?php
 require_once 'libs/Router.php';
-require_once 'app/api/api-recipe.controller.php';
+require_once 'app/api/api-comentario.controller.php';
 
 // crea el router
 $router = new Router();
 
-
 // define la tabla de ruteo
 //todos los parametros van con dos puntos siempre
-$router->addRoute('recetas', 'GET', 'ApiRecipeController', 'getAll'); //obtener todas las tareas
-$router->addRoute('recetas/:ID', 'GET', 'ApiRecipeController', 'get'); //obtener tarea.
+$router->addRoute('comentarios', 'GET', 'ApiComentarioController', 'getAll'); //obtener todos los comentarios
+$router->addRoute('comentario/:ID', 'GET', 'ApiComentarioController', 'get'); //obtener un comentario
+$router->addRoute('comentarios/:ID', 'GET', 'ApiComentarioController', 'getComentariosReceta'); //obtener c filtrados por receta
 
-$router->addRoute('recetas/:ID', 'DELETE', 'ApiRecipeController', 'delete'); 
+$router->addRoute('comentarios/:ID', 'DELETE', 'ApiComentarioController', 'delete'); //borrar
 
-$router->addRoute('recetas', 'POST', 'ApiRecipeController', 'add'); //agregar
+$router->addRoute('comentarios', 'POST', 'ApiComentarioController', 'add'); //agregar
 
-$router->addRoute('recetas/:ID', 'PUT', 'ApiRecipeController', 'update');
+/* $router->addRoute('comentarios/:ID', 'PUT', 'ApiComentarioController', 'update'); */ //actualizar
+
+$router->setDefaultRoute('ApiComentarioController', 'show404');
 
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
